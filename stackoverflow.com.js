df31696888ad2e8f78d7688a741bd03b97adbce6
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2012-01-22 23:00:07 +0100

    Add stackoverflow.com.js (kudos to @gilesbowkett)

---
+++ stackoverflow.com.js
@@ -0,0 +1,21 @@
+/*
+ * Thanks @gilesbowkett for the running start
+ */
+
+// too many nav options
+//$(".mainnavs ul").hide();
+//$("#custom-header").hide();
+$('#hlinks-user .lsep').hide();
+$('#hlinks-nav').hide();
+$('#hlinks-custom').hide();
+
+// complete assholery
+$("#overlay-header").hide();
+
+// ads
+$("#portalLink").hide();
+$(".hireme").hide();
+$("#adzerk1").hide();
+$(".everyonelovesstackoverflow").hide();
+$("#adzerk2").hide();
+$('#newsletter-ad').hide();
