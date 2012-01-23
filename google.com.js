cf37fcc33ead158b3363d4eb6fda32f0b66a1065
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2012-01-23 01:13:55 +0100

    silly temporary hack to replace logo with mario

--- google.com.js
+++ google.com.js
@@ -29,3 +29,13 @@ $('#lst-ib').bind('keydown', function(e) {
 $(document).ready(function() {
   removeRedirects();
 });
+
+/*
+ * Spice up the logo
+ */
+
+$('#logo img').attr('src', 'http://f.cl.ly/items/1a3I3A1W3J1N172c2a31/mario-180x80.jpg')
+              .attr('width', '180').attr('height', '80')
+              .css('top', '0px').css('left', '-25px');
+$('#logo').css('overflow', 'none').css('height', '73px').css('width', '180px')
+          .css('margin-left', '-25px').css('margin-top', '-20px').c;
